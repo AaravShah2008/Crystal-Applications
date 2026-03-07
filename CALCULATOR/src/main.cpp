04e9bc79d6aebec1ba3b17482ba3036867813053
CRYSTAL12345OFFICIAL <crystalcodingofficial2022@gmail.com>
2026-03-07 11:37:51 +0530

    Window generation for Calculator

--- CALCULATOR/src/main.cpp
+++ CALCULATOR/src/main.cpp
@@ -1,6 +1,37 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Crystal Calculator Engine Started!" << std::endl;
+
+    if (!glfwInit()) {
+        std::cout << "GLFW initialization failed\n";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Crystal Calculator", NULL, NULL);
+
+    if (!window) {
+        std::cout << "Window creation failed\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Load OpenGL functions using GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
